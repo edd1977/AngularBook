1529fd19004a57f421a0d559a260d8baae802bb5
denis <mai_den@mail.ru>
2019-06-23 16:30:01 +0300

    Static Angular app.

--- firstprj/src/app/app.component.ts
+++ firstprj/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Model } from './model'
 
 @Component({
-  selector: 'app-root',
+  selector: 'todo-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  
+  model = new Model();
+
+  getName() {
+    return this.model.user;
+  }
+
+  getTodoItems() {
+    return this.model.items;
+  }
+
 }
